c072c7564bd2f4fe4f44f13bb55b94513b0ebea5
pdtk <khanhpdtvn@gmail.com>
2021-05-02 16:46:33 +0700

    Add comments

--- algorithms/src/main/java/vn/khanhpdt/playgrounds/algorithms/sortings/ShellSort.java
+++ algorithms/src/main/java/vn/khanhpdt/playgrounds/algorithms/sortings/ShellSort.java
@@ -2,18 +2,19 @@ package vn.khanhpdt.playgrounds.algorithms.sortings;
 
 class ShellSort<T extends Comparable<T>> implements ComparisonSort<T> {
 
-	private int gap;
-
 	private int gapSeed;
 
 	@Override
 	public void sort(T[] items) {
 		generateGapSeed(items.length);
 
+		int gap;
 		do {
-			generateGap();
+			// split the array into multiple subarrays
+			// gap is also the number of subarrays
+			gap = generateGap();
 
-			// when i == gap, all the elements in the array are processed
+			// each iteration in this for-loop sorts one subarray
 			for (int i = 0; i < gap; i++) {
 				// insertion-sort each subarray containing elements separated by gap-position
 				for (int j = i + gap; j < items.length; j = j + gap) {
@@ -35,16 +36,17 @@ class ShellSort<T extends Comparable<T>> implements ComparisonSort<T> {
 	/**
 	 * @see <a href="https://en.wikipedia.org/wiki/Shellsort#Gap_sequences"></a>
 	 */
-	private void generateGap() {
-		gap = (int) ((Math.pow(3, gapSeed) - 1) / 2);
+	private int generateGap() {
+		int gap = (int) ((Math.pow(3, gapSeed) - 1) / 2);
 		gapSeed--;
+		return gap;
 	}
 
 	/**
 	 * @see <a href="https://en.wikipedia.org/wiki/Shellsort#Gap_sequences"></a>
 	 */
 	private void generateGapSeed(int nElements) {
-		gapSeed = (int) Math.floor(Math.log(2 * Math.ceil(nElements / 3) + 1) / Math.log(3));
+		gapSeed = (int) Math.floor(Math.log(2 * Math.ceil(((double) nElements) / 3) + 1) / Math.log(3));
 	}
 
 }
